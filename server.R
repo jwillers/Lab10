225c0bf11bc04ec31be558a5a582a3aa625ed052
JMarieShort <JMarieShort@gmail.com>
2014-03-28 15:21:53 -0500

    Final Version

--- server.R
+++ server.R
@@ -1,37 +1,63 @@
-library("shiny")
+library(shiny)
+library(maps)
+library(reshape)
 
-data("us.cities")
-us.cities$country.etc<-toupper(us.cities$country.etc)
-StateList<-sort(unique(us.cities$country.etc))
-us.cities$name<-substr(us.cities$name,1,nchar(us.cities$name)-3)
+## Prepare the data
+data("us.cities") 
+# Rename variables in the us.cities dataset
+us.cities <- rename(us.cities, c(name="city"))
+us.cities <- rename(us.cities, c(country.etc="state"))
+# Make all state codes uniform
+us.cities$state<-toupper(us.cities$state)
+# Create a list of sorted unique state codes
+StateList<-sort(unique(us.cities$state))
+# Remove Alaska and Hawaii
+StateList <- subset(StateList, !(StateList%in%c("HI","AK")))
+# Remove the state codes at the end of the city names
+us.cities$city<-substr(us.cities$city,1,nchar(us.cities$city)-3)
+# Initialize a data frame with Ames, IA
+datalist <- subset(us.cities,city == "Ames" & state == 'IA')
 
-datalist <- head(us.cities,2)
-# Define server logic required to summarize and view the selected dataset
+## The main function for the map
 shinyServer(function(input, output, session) {
+  # Take the data frame and make a reactive counterpart
+  values<-reactiveValues()
+  values$datalist<-datalist
   
+  # Watch the state selector and if changed, update the cityset
   observe({
-    cities <- subset(us.cities,country.etc == input$stateset)$name
+    cities <- subset(us.cities,state == input$stateset)$city
     updateSelectInput(session, "cityset", choices = cities)  
   })
   
+  # Watch the updateData action button and change the data frame if clicked
   observe({
-    input$updateData
-    datalist<-rbind(datalist,subset(us.cities, name == input$cityset))
+    # This just watches for a click
+    input$updateData 
+    # Update the dataframe, but isolate so it does trigger on
+    # values$datalist (This prevents an infinite loop),
+    # input$cityset or input$stateset so that points aren't added until the button is pushed
+    values$datalist<-rbind(isolate(values$datalist),subset(us.cities, city == isolate(input$cityset)&state == isolate(input$stateset)))
   })
   
+  # Make the map
   output$mapUSA <- renderPlot({
     map('usa')
-    if (nrow(datalist)>0) {
-      points(datalist$long, datalist$lat)
-      lines(datalist$long,datalist$lat)
+    # Make sure to show only when we have data
+    if (nrow(values$datalist)>0) {
+      # Make the point, then draw the lines
+      points(values$datalist$long, values$datalist$lat)
+      lines(values$datalist$long, values$datalist$lat)
     }  
+    # When a city is selected, show the point without the line
     if (input$cityset != "") {
-      citCoord<-unlist(subset(us.cities,name == input$cityset,select=c(long,lat)))
+      citCoord<-unlist(subset(us.cities,city == input$cityset,select=c(long,lat)))
       points(citCoord[[1]], citCoord[[2]])
     }
   })
+  # Show the list of cities only if one has been added
   output$view <- renderTable({
-    datalist
+    if(nrow(values$datalist)>0) values$datalist[,1:5]
   })
   
 })
--- ui.R
+++ ui.R
@@ -1,33 +1,41 @@
 library(shiny)
 library(maps)
 library(ggplot2)
+library(reshape)
 
-data("us.cities")
-us.cities$country.etc<-toupper(us.cities$country.etc)
-StateList<-sort(unique(us.cities$country.etc))
+## Prepare the data
+data("us.cities") 
+# Rename variables in the us.cities dataset
+us.cities <- rename(us.cities, c(name="city"))
+us.cities <- rename(us.cities, c(country.etc="state"))
+# Make all state codes uniform
+us.cities$state<-toupper(us.cities$state)
+# Create a list of sorted unique state codes
+StateList<-sort(unique(us.cities$state))
+# Remove Alaska and Hawaii
+StateList <- subset(StateList, !(StateList%in%c("HI","AK")))
+# Remove the state codes at the end of the city names
+us.cities$city<-substr(us.cities$city,1,nchar(us.cities$city)-3) 
 
 
-# Define UI for dataset viewer application
+# Define UI for Route Map
 shinyUI(fluidPage(
   
   # Application title
   titlePanel("Choose a route"),
   
-  # Sidebar with controls to select a dataset and specify the
-  # number of observations to view
+  # Sidebar with controls to select a city/state/Add to Route
   sidebarLayout(
     sidebarPanel(
-      selectInput("stateset", "Choose a state:", 
-                  choices = StateList),
+      selectInput("stateset", "Choose a state:", choices = StateList),
       
-      selectInput("cityset", "Choose a city:", choices = "First, select a state"),
+      selectInput("cityset", "Choose a city:", choices = "Select a state first"),
       
       actionButton("updateData","Add to list")
       
     ),
-        
-   # Show a summary of the dataset and an HTML table with the 
-   # requested number of observations
+    
+    # Show a map of the US with a list of the cities chosen
     mainPanel(
       plotOutput("mapUSA"),
       tableOutput("view")
